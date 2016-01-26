b46af9af237e671bfe287dd0c1cf5f6d20b520f1
przemol <pstempor@gmail.com>
2016-01-26 15:36:08 +0000

    Update to R 3.2.3 Patched (2016/01/25, r70000) for snowleopard from r.research.att.com

--- SeqPlots.app/Contents/Resources/Rbin/Versions/3.1/Resources/library/seqplots/doc/QuickStart.R
+++
@@ -1,6 +0,0 @@
-## ----style, echo = FALSE, results = 'asis'-------------------------------
-BiocStyle::markdown()
-
-## ----echo=FALSE----------------------------------------------------------
-sessionInfo()
-
